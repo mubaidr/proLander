419c052b561c53b2ef2e735b678dedae6002897f
Usman Khwaja <numbfall@live.com>
2014-11-07 16:06:39 +0800

    inputs are up

--- plander.js
+++ plander.js
@@ -11,10 +11,26 @@ function preload() {
 
 function create() {
     background = game.add.tileSprite(0, 0, 800, 600, "background");
-    craft = game.add.sprite(350, 0, "lander0");
-    //craft.frame = 0;
+    craft = game.add.sprite(350, 0, "lander0", 0);
+    game.physics.startSystem(craft, Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(craft);
+    game.physics.arcade.gravity.y = 18;
+    craft.body.collideWorldBounds = true;
 }
 
 function update() {
-
+    if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+        craft.body.acceleration.y -= 1;
+        craft.frame = 1;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+        craft.body.acceleration.x += 0.9;
+        craft.frame = 2;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+        craft.body.acceleration.x -= 0.9;
+        craft.frame = 3;
+    } else {
+        craft.body.acceleration.y = 0;
+        craft.body.acceleration.x = 0;
+        craft.frame = 0;
+    }
 }
