bb0c4a0bf7e7a64b71e3a649280f95763d4dda0d
Usman Khwaja <numbfall@live.com>
2014-11-10 05:36:29 +0800

    bug fixes, sound, velocity

--- plander.js
+++ plander.js
@@ -8,21 +8,20 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'play', {
 function preload() {
     game.load.image("background", "images/2.jpg");
     game.load.spritesheet("lander0", "images/lander.png", 80, 79, 4);
-    game.load.image("base", "images/base.png")
+    game.load.image("base", "images/base.png");
     game.load.audio('clap', 'sounds/clap.ogg');
     game.load.audio('fail', 'sounds/fail.ogg');
 }
 
 function create() {
     r = game.rnd.integerInRange(0, 700);
-    ground = 614;
     tank = 100;
     background = game.add.tileSprite(0, 0, 800, 600, "background");
     craft = game.add.sprite(350, 0, "lander0", 0);
     pad = game.add.sprite(r, 580, "base");
     win = game.add.audio('clap');
     lose = game.add.audio('fail');
-    game.world.bounds.setTo(-10, 0, 820, ground);
+    game.world.bounds.setTo(-810, -1000, 1820, 1614);
     game.physics.setBoundsToWorld();
     game.physics.startSystem(craft, Phaser.Physics.ARCADE);
     game.physics.arcade.enable(craft);
@@ -30,36 +29,36 @@ function create() {
     craft.body.collideWorldBounds = true;
     velocity = game.add.text(20, 20, "", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     distance = game.add.text(20, 35, "", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     fuel = game.add.text(20, 50, "", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     keyH = game.add.text(734, 20, "Down: H", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     keyK = game.add.text(745, 35, "Left: K", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     keyL = game.add.text(738, 50, "Right: L", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     keyR = game.add.text(727, 65, "Restart: R", {
         font: "12px Arial",
-        fill: "#ffffff",
+        fill: "#ffffff"
     });
     landed = game.add.text(336, 70, "", {
         font: "24px Arial",
         fill: "#ffffff",
-        align: "center",
+        align: "center"
     });
 }
 
@@ -68,7 +67,6 @@ function update() {
         game.state.start(game.state.current);
     }
     if (!craft.body.onFloor() && tank > 0) {
-        speed = craft.body.velocity.y;
         if (game.input.keyboard.isDown(Phaser.Keyboard.H)) {
             craft.body.acceleration.y -= 1;
             craft.frame = 1;
@@ -89,23 +87,21 @@ function update() {
     } else if (!craft.body.onFloor() && tank <= 0) {
         craft.body.acceleration.y = 18;
         craft.frame = 0;
-    } else if (craft.body.onFloor() && tank > 0) {
-        speed = speed;
-        craft.body.immovable = true;
-        craft.body.velocity.x = 0;
+    } else if (craft.body.onFloor() && (tank > 0 || tank <= 0)) {
         craft.frame = 0;
-        if (craft.body.position.x >= (r - 10) && craft.body.position.x <= (r + 30) && speed < 20) {
+        craft.body.moves = false;
+        if (craft.body.position.x >= (r - 10) && craft.body.position.x <= (r + 30) && craft.body.velocity.y < 20) {
             win.play();
             landed.setText("Safe Landing");
         } else {
-            landed.setText("  Crashed   ");
             lose.play();
+            landed.setText("  Crashed   ");
         }
     }
 }
 
 function render() {
-    velocity.setText("Velocity: " + speed.toFixed(2));
-    distance.setText("Distance: " + ((ground - 79) - craft.body.position.y.toFixed(0)));
+    velocity.setText("Velocity: " + craft.body.speed.toFixed(2));
+    distance.setText("Distance: " + game.physics.arcade.distanceToXY(craft, (r + 15), 535).toFixed(2));
     fuel.setText("Fuel: " + tank.toFixed(0));
 }
